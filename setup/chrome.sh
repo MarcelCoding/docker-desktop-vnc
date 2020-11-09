bea1f82f339e44b101e7b1501d1f514a361d032d
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2020-11-09 15:02:38 +0100

    Initial Commit

---
+++ setup/chrome.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# every exit != 0 fails the script
+#set -e
+
+# see: https://github.com/felbinger/PostInstall/blob/master/scripts/installGoogleChrome.sh
+
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+  echo "Please run the script as root!"
+else
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -i google-chrome-stable_current_amd64.deb
+  rm google-chrome-stable_current_amd64.deb
+  apt-get install --no-install-recommends -y -f
+fi
