c3d1b532242989278520f51412365cbef452b030
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2020-11-09 16:05:39 +0100

    Added IntelliJ

---
+++ setup/idea.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# every exit != 0 fails the script
+set -e
+
+function installPlugin() {
+  wget -O plugin.zip "$1"
+  unzip plugin.zip -d /opt/idea/plugins
+  rm plugin.zip
+}
+
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+  echo "Please run the script as root!"
+else
+  mkdir -p /opt/idea
+  wget -O- "https://download-cf.jetbrains.com/idea/ideaIU-2020.3.tar.gz" | tar -xzvC /opt/idea --strip 1
+
+  # .ignore
+  installPlugin "https://plugins.jetbrains.com/files/7495/105230/.ignore-4.0.0.zip"
+
+  # GO & GO Templates
+  installPlugin "https://plugins.jetbrains.com/files/9568/104013/go-203.5981.155.zip"
+  installPlugin "https://plugins.jetbrains.com/files/10581/104141/go-template-203.5981.152.zip"
+
+  # PHP
+  installPlugin "https://plugins.jetbrains.com/files/6610/104391/php-203.5981.175.zip"
+
+  # Python (PyCharm IDE Professional) - (Community is another plugin)
+  installPlugin "https://plugins.jetbrains.com/files/631/104351/python-203.5981.165.zip"
+
+  # PowerShell
+  installPlugin "https://plugins.jetbrains.com/files/10249/94564/PowerShell-2.0.5.zip"
+
+  mkdir -p "/home/${USER}/.local/share/applications"
+  cat <<EOF >"/home/${USER}/.local/share/applications/jetbrains-idea.desktop"
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=IntelliJ IDEA Ultimate Edition
+Icon=/opt/idea/bin/idea.svg
+Exec="/opt/idea/bin/idea.sh" %f
+Comment=Capable and Ergonomic IDE for JVM
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-idea
+EOF
+
+fi
