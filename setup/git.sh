2b9eb96c5ddeb1b6ba9f8c6629eba1907278ac18
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2020-11-09 16:12:30 +0100

    Added git

--- setup/git.sh
+++ setup/git.sh
@@ -6,5 +6,5 @@ set -e
 if [[ $(/usr/bin/id -u) != "0" ]]; then
   echo "Please run the script as root!"
 else
-  wget -O- "https://download-cf.jetbrains.com/idea/ideaIU-2020.2.3.tar.gz" | tar -xzC /opt
+  apt-get -y --no-install-recommends install git
 fi
