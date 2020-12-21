101f28c26eb0f175a1ff38c9eb59596f2c461b3e
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2020-12-21 21:24:30 +0100

    Added go

---
+++ setup/go.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# every exit != 0 fails the script
+set -e
+
+if [[ $(/usr/bin/id -u) != "0" ]]; then
+  echo "Please run the script as root!"
+else
+  wget -O- https://golang.org/dl/go1.15.6.linux-amd64.tar.gz | tar -xzvC /usr/local
+  echo "export PATH=\$PATH:/usr/local/go/bin" >>"/home/${USER}/.bashrc"
+fi
