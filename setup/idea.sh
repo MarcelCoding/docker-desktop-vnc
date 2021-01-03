71849786ae4bd9dcaf119a324c5b267b75461306
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-01-03 11:15:58 +0100

    Fixed IntelliJ Rust Support

--- setup/idea.sh
+++ setup/idea.sh
@@ -32,7 +32,10 @@ else
   installPlugin "https://plugins.jetbrains.com/files/10249/94564/PowerShell-2.0.5.zip"
 
   # Rust
-  installPlugin "https://plugins.jetbrains.com/files/8182/105410/intellij-rust-0.3.137.3543-203.zip"
+  installPlugin "https://plugins.jetbrains.com/files/8182/106545/intellij-rust-0.3.138.3572-203.zip"
+
+  # Toml
+  installPlugin "https://plugins.jetbrains.com/files/8195/106546/intellij-toml-0.2.138.3572-203.zip"
 
   mkdir -p "/home/${USER}/.local/share/applications"
   cat <<EOF >"/home/${USER}/.local/share/applications/jetbrains-idea.desktop"
