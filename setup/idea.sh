bcd5bacda3a88ca3a8b3307bc2e9771c0d7cb096
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-02-22 19:10:47 +0100

    Updated IntelliJ and plugins

--- setup/idea.sh
+++ setup/idea.sh
@@ -13,29 +13,29 @@ if [[ $(/usr/bin/id -u) != "0" ]]; then
   echo "Please run the script as root!"
 else
   mkdir -p /opt/idea
-  wget -O- "https://download-cf.jetbrains.com/idea/ideaIU-2020.3.1.tar.gz" | tar -xzvC /opt/idea --strip 1
+  wget -O- "https://download-cf.jetbrains.com/idea/ideaIU-2020.3.2.tar.gz" | tar -xzvC /opt/idea --strip 1
 
   # .ignore
-  installPlugin "https://plugins.jetbrains.com/files/7495/106136/.ignore-4.0.2.zip"
+  installPlugin "https://plugins.jetbrains.com/files/7495/107565/.ignore-4.0.3.zip"
 
   # GO & GO Templates
-  installPlugin "https://plugins.jetbrains.com/files/9568/106609/go-203.6682.168.zip"
-  installPlugin "https://plugins.jetbrains.com/files/10581/104141/go-template-203.5981.152.zip"
+  installPlugin "https://plugins.jetbrains.com/files/9568/110521/go-211.5787.15.zip"
+  installPlugin "https://plugins.jetbrains.com/files/10581/110427/go-template-211.5787.4.zip"
 
   # PHP
-  installPlugin "https://plugins.jetbrains.com/files/6610/106348/php-203.6682.137.zip"
+  installPlugin "https://plugins.jetbrains.com/files/6610/110589/php-211.5787.18.zip"
 
   # Python (PyCharm IDE Professional) - (Community is another plugin)
-  installPlugin "https://plugins.jetbrains.com/files/631/106697/python-203.6682.179.zip"
+  installPlugin "https://plugins.jetbrains.com/files/631/110525/python-211.5787.15.zip"
 
   # PowerShell
   installPlugin "https://plugins.jetbrains.com/files/10249/94564/PowerShell-2.0.5.zip"
 
   # Rust
-  installPlugin "https://plugins.jetbrains.com/files/8182/106545/intellij-rust-0.3.138.3572-203.zip"
+  installPlugin "https://plugins.jetbrains.com/files/8182/110860/intellij-rust-0.3.141.3674-203.zip"
 
   # Toml
-  installPlugin "https://plugins.jetbrains.com/files/8195/106546/intellij-toml-0.2.138.3572-203.zip"
+  installPlugin "https://plugins.jetbrains.com/files/8195/110862/intellij-toml-0.2.141.3674-203.zip"
 
   mkdir -p "/home/${USER}/.local/share/applications"
   cat <<EOF >"/home/${USER}/.local/share/applications/jetbrains-idea.desktop"
