5a70fbc4df0c145206dece3f7670131e5091fc3c
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-12-26 21:03:58 +0100

    Updated go to v1.17

--- setup/go.sh
+++ setup/go.sh
@@ -6,6 +6,6 @@ set -e
 if [[ $(/usr/bin/id -u) != "0" ]]; then
   echo "Please run the script as root!"
 else
-  wget -O- https://golang.org/dl/go1.16.6.linux-amd64.tar.gz | tar -xzvC /usr/local
+  wget -O- https://go.dev/dl/go1.17.5.linux-amd64.tar.gz | tar -xzvC /usr/local
   echo "export PATH=\$PATH:/usr/local/go/bin" >>"/home/${USER}/.bashrc"
 fi
